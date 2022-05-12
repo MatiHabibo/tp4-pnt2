f40fcdba45c0c53e10e17fcc24e05a2c8f07c5ef
Matias Habibo <matihabibo@gmail.com>
2022-05-12 15:16:55 -0300

    punto 1 terminado, a corregir tabla

---
+++ src/components/Formulario.vue
@@ -0,0 +1,127 @@
+<template>
+
+  <section class="src-components-formulario">
+    <div class="jumbotron">
+    <h1>Formulario</h1>
+
+    <vue-form :state="formState" @submit.prevent="send()">
+ 
+    <validate tag="div">
+      <label for="nombre">Nombre</label>
+      <input type="text" class="form-control" autocomplete="off" v-model.trim="formData.nombre" required name="nombre" :minlength="nomMin" :maxlength="nomMax"/>
+ 
+      <field-messages name="nombre" show="$dirty">
+        <div slot="required" class="alert alert-danger">Nombre requerido</div>
+        <div slot="minlength" class="alert alert-danger">Minimo {{ nomMin }} letras</div>
+        <div slot="maxlength" class="alert alert-danger">Maximo {{ nomMax }} letras</div>
+      </field-messages>
+    </validate>
+  <br>
+      <validate tag="div">
+      <label for="edad">Edad</label>
+      <input type="number" name="edad" class="form-control" autocomplete="off" v-model.number="formData.edad" required :min="edMin" :max="edMax"/>
+ 
+      <field-messages name="edad" show="$dirty">
+        <div slot="required" class="alert alert-danger">Nombre requerido</div>
+        <div slot="min" class="alert alert-danger">Edad minima {{ edMin }}</div>
+        <div slot="max" class="alert alert-danger">Edad Maxima {{ edMax }}</div>
+      </field-messages>
+    </validate>
+  <br>
+    <validate tag="div">
+      <label for="email">Email</label>
+      <input v-model.trim="formData.email" name="email" autocomplete="off" type="email" required />
+ 
+      <field-messages name="email" show="$dirty">
+        <div slot="required" class="alert alert-danger">Email requerido</div>
+        <div slot="email" class="alert alert-danger">Ingrese un email valido</div>
+      </field-messages>
+    </validate>
+ 
+    <button class="btn btn-danger my-3" :disabled="formState.$invalid">Enviar</button>
+  </vue-form>
+
+  <table class="table table-dark" id="myTable">
+     <thead>
+        <tr>
+          <th>Nombre</th>
+          <th>Edad</th>
+          <th>Mail</th>
+        </tr>
+     </thead>
+     <tbody>
+       <tr>
+        <div v-if="(formState.$valid)">
+          <td>{{ formData.nombre}}</td>
+          <td>{{ formData.edad}} </td>
+          <td>{{ formData.email}}</td>
+        </div>
+        </tr>
+    </tbody>
+   </table>
+
+<br>
+  <!-- <p>Form data</p>
+  <pre>{{ formData }}</pre>
+  
+  <p>Form state</p>
+  <pre>{{ formState }}</pre> -->
+  </div>
+  </section>
+
+</template>
+
+<script lang="js">
+
+  export default  {
+    name: 'src-components-formulario',
+    props: [],
+    mounted () {
+
+    },
+    data () {
+      return {
+        formState : {},
+        formData : this.initialData(),
+        nomMin: 5,
+        nomMax : 15,
+        edMin: 18,
+        edMax: 120
+      }
+    },
+    methods: {
+      initialData(){
+        return {
+          nombre : '',
+          edad: '',
+          email: ''
+        }
+      },
+      send(){
+        this.formData = this.initialData() 
+        this.formState._reset() 
+      },
+      appendData(){
+          
+      }
+    },
+    computed: {
+
+    }
+}
+
+
+</script>
+
+<style scoped lang="css">
+  .src-components-formulario {
+
+  }
+
+  .jumbotron{
+    background-color: green;
+  }
+    pre {
+    color: black;
+  }
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,73 @@
+<template>
+
+  <section class="src-components-nav-bar">
+    <h1>src-components-nav-bar Component</h1>
+
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <!-- <div class="container-fluid"> -->
+      <router-link to="/formulario">
+        <a class="navbar-brand" href="#">Inicio</a>
+      </router-link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link to="/formulario">
+              <a class="nav-link" href="#">Formulario</a>
+            </router-link>
+          </li>
+          <!-- <li class="nav-item">
+            <router-link to="/elOtro">
+              <a class="nav-link" href="#">El otro</a>
+            </router-link>
+          </li> -->
+
+        </ul>
+      </div>
+      <!-- </div> -->
+    </nav>
+
+  </section>
+
+</template>
+
+<script lang="js">
+
+  export default  {
+    name: 'src-components-nav-bar',
+    props: [],
+    mounted () {
+
+    },
+    data () {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+
+
+</script>
+
+<style scoped lang="css">
+  .src-components-nav-bar {
+
+  }
+</style>
